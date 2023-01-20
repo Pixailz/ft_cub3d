da88d57a0b5a810a9e7c8b79bbfe6e0456d79c77
Pixailz <pixailz1@gmail.com>
2023-01-20 17:11:02 +0100

    :construction: clean

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 23:57:29 by brda-sil          #+#    #+#             */
-/*   Updated: 2023/01/18 10:03:47 by brda-sil         ###   ########.fr       */
+/*   Updated: 2023/01/20 17:10:36 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,6 @@ int	main(int argc, char **argv)
 	t_main			config;
 
 	if (argc != 2)
-		return (ft_printf_fd(2, "Error: program take one arguments.\n"));
-	init_config(&config);
-	if (parse_entry(&config.err, argv[1], &config.parsing))
-		return (error_print(config.err, &config));
-	start_rendering(&config);
-	error_print(config.err, &config);
-	return (have_error(config.err, 0));
+		return (ft_printf_fd(2, "Error:\nprogram take one arguments.\n"));
+	return (0);
 }

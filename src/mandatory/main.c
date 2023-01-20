0915bd87637b25c10a14768b68b1cbf91afbcfc0
Pixailz <pixailz1@gmail.com>
2023-01-20 17:16:31 +0100

    :package: main.c - working base main

--- src/mandatory/main.c
+++ src/mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: brda-sil <brda-sil@students.42angouleme    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 23:57:29 by brda-sil          #+#    #+#             */
-/*   Updated: 2023/01/20 17:10:36 by brda-sil         ###   ########.fr       */
+/*   Updated: 2023/01/20 17:15:51 by brda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,10 @@ int	main(int argc, char **argv)
 
 	if (argc != 2)
 		return (ft_printf_fd(2, "Error:\nprogram take one arguments.\n"));
-	return (0);
+	init_config(&config);
+	if (parse_entry(&config.err, argv[1], &config.parsing))
+		return (error_print(config.err, &config));
+	start_rendering(&config);
+	error_print(config.err, &config);
+	return (have_error(config.err, 0));
 }
